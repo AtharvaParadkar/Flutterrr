e48000d24ad430c6f82c1e7d3dc9524fddbca4ef
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-08-22 22:59:27 +0530

    QR Code Link Launcher

--- lib/widgets/qrcodescanner.dart
+++ lib/widgets/qrcodescanner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QRCodeScanner extends StatefulWidget {
   const QRCodeScanner({super.key});
@@ -12,6 +13,14 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  // ignore: unused_field
+  Future<void>? _launched;
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
 
   @override
   void reassemble() {
@@ -19,6 +28,34 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
     if (controller != null) {
       controller!.pauseCamera();
       controller!.resumeCamera();
+    }
+  }
+
+  void onQRCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+        print(result!.code);
+        final uri = Uri.parse(result!.code!);
+        print(uri);
+        _launchInBrowserView(uri);
+      });
+    });
+  }
+
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Future<void> _launchInBrowserView(Uri url) async {
+    if (!await launchUrl(url, mode: LaunchMode.inAppBrowserView)) {
+      throw Exception('Could not launch $url');
     }
   }
 
@@ -48,37 +85,41 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
               ),
             ),
           ),
-          Expanded(
-            child: Center(
-              child: result != null
-                  ? Text(
-                      '${result!.code}',
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center,
-                    )
-                  : Text(
-                      'Scan a code',
-                      style: TextStyle(fontSize: 18),
-                    ),
-            ),
+          SizedBox(height: 30),
+          Center(
+            child: result != null
+                ? Text(
+                    '${result!.code}',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  )
+                : Text(
+                    'Scan a code',
+                    style: TextStyle(fontSize: 18),
+                  ),
           ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Launch in Browser'),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Launch in App'),
+          ),
+          SizedBox(height: 30),
+          FutureBuilder<void>(future: _launched, builder: _launchStatus),
         ],
       ),
     );
   }
 
-  void onQRCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    controller!.dispose();
-    super.dispose();
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
   }
 }
