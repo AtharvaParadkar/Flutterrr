34a42a762314deaa440dfcf4ffddc46a405d7a75
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-08-22 14:27:59 +0530

    QR Code Scanner

--- lib/Screens/jrewards.dart
+++ lib/Screens/jrewards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/widgets/qrcodescanner.dart';
 
 class JayRewardsPage extends StatefulWidget {
   const JayRewardsPage({super.key});
@@ -23,6 +24,47 @@ class _JayRewardsPageState extends State<JayRewardsPage> {
           SizedBox(width: 20),
           Icon(Icons.shopping_bag_outlined, size: 30),
           SizedBox(width: 20),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 100,
+              width: 200,
+              child: Card(
+                elevation: 15,
+                shadowColor: Color.fromARGB(183, 255, 0, 0),
+                color: Color.fromARGB(255, 255, 0, 0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Rewards',
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                      ),
+                      Text(
+                        'Scan QR for Rewards 👇',
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          InkWell(
+            onTap: () => Navigator.push(
+                context, MaterialPageRoute(builder: (_) => QRCodeScanner())),
+            child: Icon(
+              Icons.qr_code_scanner,
+              size: 50,
+              color: Color.fromARGB(255, 0, 255, 0),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/qrcodescanner.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRCodeScanner extends StatefulWidget {
+  const QRCodeScanner({super.key});
+
+  @override
+  State<QRCodeScanner> createState() => _QRCodeScannerState();
+}
+
+class _QRCodeScannerState extends State<QRCodeScanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      controller!.pauseCamera();
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 37,
+          width: 95,
+          child: Image.asset('assets/images/jaylogo.jpg', fit: BoxFit.fill),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: onQRCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.blue,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: 300,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: result != null
+                  ? Text(
+                      '${result!.code}',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    )
+                  : Text(
+                      'Scan a code',
+                      style: TextStyle(fontSize: 18),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onQRCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+}
