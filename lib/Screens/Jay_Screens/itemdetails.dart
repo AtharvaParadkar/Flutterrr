ebd579c1e5f76d77a364bb239091892449c1aecc
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-10-14 10:38:05 +0530

    Item Details

--- lib/Screens/Jay_Screens/itemdetails.dart
+++ lib/Screens/Jay_Screens/itemdetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/modal/jaymodal.dart';
+import 'package:gap/gap.dart';
 
 // ignore: must_be_immutable
 class ItemDetails extends StatefulWidget {
@@ -21,20 +22,86 @@ class _ItemDetailsState extends State<ItemDetails> {
           child: Image.asset('assets/images/jaylogo.jpg', fit: BoxFit.fill),
         ),
       ),
-      body: Container(
-        height: 300,
-        width: 300,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Card(
-              elevation: 7,
-              child: Image.network(widget.item.ItemImageUrl, fit: BoxFit.cover),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 250,
+            width: MediaQuery.sizeOf(context).width,
+            child: Image.network(widget.item.ItemImageUrl, fit: BoxFit.contain),
+          ),
+          Gap(20),
+          Text(widget.item.ItemTitle,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+          Gap(10),
+          Text('\u{20B9} ${widget.item.ItemPrice.toString()}',style: TextStyle(fontSize: 20),),
+          Gap(15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Container(
+            height: 40,
+            width: MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [
+                  Color.fromARGB(200, 255, 0, 0),
+                  Color.fromARGB(150, 255, 0, 0),
+                  Color.fromARGB(200, 255, 0, 0),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(15),
             ),
-            Text(widget.item.ItemTitle),
-            Text('${widget.item.ItemPrice.toString()}'),
-          ],
-        ),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+              ),
+              child: const Text(
+                'Add to Cart',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+                    ),
+          ),
+          Gap(15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Container(
+            height: 40,
+            width: MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [
+                  Color.fromARGB(200, 255, 0, 0),
+                  Color.fromARGB(150, 255, 0, 0),
+                  Color.fromARGB(200, 255, 0, 0),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+              ),
+              child: const Text(
+                'Buy Now',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+                    ),
+          ),
+        ],
       ),
     );
   }
